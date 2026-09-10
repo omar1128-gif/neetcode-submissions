3b3cc4c1ead116dbda07091d103f1194a86b1741
Omar Fathy <54510800+omar1128-gif@users.noreply.github.com>
2026-09-10 14:55:34 +0300

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = "";
+        for (const str of strs) {
+            result += str.length + "#" + str;
+        }
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const result = [];
+        let i = 0;
+
+        while (i < str.length) {
+            let j = i;
+
+            while (str[j] !== "#") j++;
+
+            const length = parseInt(str.substring(i, j), 10);
+
+            i = j + 1;
+
+            result.push(str.substring(i, i + length));
+
+            i += length;
+        }
+        return result;
+    }
+}
