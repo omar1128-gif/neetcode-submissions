b2f0ecb02cc5165bee696afb7350e8d9cee81f49
Omar Fathy <54510800+omar1128-gif@users.noreply.github.com>
2026-09-10 22:02:45 +0300

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        const charSet = new Set();
+        let left = 0;
+        let maxLength = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            while (charSet.has(s[right])) {
+                charSet.delete(s[left]);
+                left++;
+            }
+
+            charSet.add(s[right]);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
