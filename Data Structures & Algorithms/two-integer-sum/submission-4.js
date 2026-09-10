bcbd38ac55ba26a5f13b996b4fe47c8baaf49da0
Omar Fathy <54510800+omar1128-gif@users.noreply.github.com>
2026-09-10 03:03:50 +0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+
+            if (complement in map) {
+                return [map[complement], i];
+            }
+
+            map[nums[i]] = i;
+        }
+        return [];
+    }
+}
