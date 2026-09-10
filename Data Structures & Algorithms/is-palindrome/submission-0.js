af3a638ec28c5810d9337f3f1cd7e0782bd85889
Omar Fathy <54510800+omar1128-gif@users.noreply.github.com>
2026-09-10 16:16:02 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    isAlphaNumeric(char) {
+        const code = char.charCodeAt(0);
+        return (
+            (code >= 48 && code <= 57) || // 0-9
+            (code >= 65 && code <= 90) || // A-Z
+            (code >= 97 && code <= 122) // a-z
+        );
+    }
+
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            while (left < right && !this.isAlphaNumeric(s[left])) left++;
+            while (left < right && !this.isAlphaNumeric(s[right])) right--;
+
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
